8f03b8934d582a353ca87c214ad09789a8388522
Yousef Saeed <yousafesaeed@gmail.com>
2023-12-05 11:10:55 +0200

    0x01 - Task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    visited_boxes = set()
+    keys = [0]
+
+    while keys:
+        current_key = keys.pop()
+        visited_boxes.add(current_key)
+
+        for box_key in boxes[current_key]:
+            if box_key not in visited_boxes and 0 <= box_key < len(boxes):
+                keys.append(box_key)
+
+    return len(visited_boxes) == len(boxes)
