300ed4a82015a699da2f2d48bf4bf84bf034d5f4
Yousef Saeed <yousafesaeed@gmail.com>
2023-12-05 11:11:08 +0200

    0x01 - Test 0

--- 0x01-lockboxes/tests/0-main.py
+++ 0x01-lockboxes/tests/0-main.py
@@ -4,3 +4,14 @@ import sys
 import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+canUnlockAll = __import__('0-lockboxes').canUnlockAll
+
+boxes = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes))
