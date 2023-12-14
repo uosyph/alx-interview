45c7d985b0e584d92a2132f9a9ad8db6d46c14f9
Yousef Saeed <yousafesaeed@gmail.com>
2023-12-14 17:46:21 +0200

    0x02 - 0: Create func logic

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,3 +9,16 @@ def minOperations(n):
 
     if not isinstance(n, int) or n < 2:
         return 0
+
+    current_factor = 2
+    prime_factors_list = []
+    remaining_value = n
+
+    while remaining_value != 1:
+        if remaining_value % current_factor == 0:
+            while remaining_value % current_factor == 0:
+                remaining_value //= current_factor
+                prime_factors_list.append(current_factor)
+        current_factor += 1
+
+    return sum(prime_factors_list)
