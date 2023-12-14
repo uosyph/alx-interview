3907b9b0cf747eda57a88d1e6f639c3fd529927d
Yousef Saeed <yousafesaeed@gmail.com>
2023-12-14 17:50:22 +0200

    0x02 - 0: Create docstring

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,10 +1,32 @@
 #!/usr/bin/python3
 """
+Calculate the minimum number of operations required to transform a given
+positive integer 'n' into 1. An operation can be either dividing the number
+by its smallest prime factor or incrementing the number.
+
+Parameters:
+- n (int): The positive integer to be transformed.
+
+Returns:
+int: The minimum number of operations required to transform 'n' into 1.
+
+Note:
+- If 'n' is not a positive integer or less than 2, the function returns 0.
+- The function uses prime factorization to find the
+smallest prime factors of 'n'.
 """
 
 
 def minOperations(n):
     """
+    Calculate the minimum number of operations required to
+    transform a given positive integer 'n' into 1.
+
+    Parameters:
+    - n (int): The positive integer to be transformed.
+
+    Returns:
+    int: The minimum number of operations required to transform 'n' into 1.
     """
 
     if not isinstance(n, int) or n < 2:
