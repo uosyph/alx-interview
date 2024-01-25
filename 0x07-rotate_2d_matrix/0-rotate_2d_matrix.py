4fc261770bd64a8e782433b47a80cb05daf9c488
Yousef Saeed <yousafesaeed@gmail.com>
2024-01-25 13:02:44 +0200

    0x07 - Task 0

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""Define function to rotate a 2D matrix."""
+
+
+def rotate_2d_matrix(matrix):
+    """Rotate a 2D matrix in-place.
+
+    Parameters:
+        - matrix (list of lists): The 2D matrix to be rotated.
+    """
+    size = len(matrix)
+    rotated_order = []
+
+    # Traverse the matrix in a clockwise direction and store the elements
+    for i in range(size):
+        for j in range(size - 1, -1, -1):
+            rotated_order.append(matrix[j][i])
+
+    # Populate the matrix with the rotated elements
+    for i in range(size):
+        for j in range(size):
+            matrix[i][j] = rotated_order.pop(0)
